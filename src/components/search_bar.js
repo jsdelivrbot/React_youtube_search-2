5ae3bb8a53c62ea73e0b5fc5f3efddbd9358f2cf
Tanim Mahmud <tanimmahmud08@gmail.com>
2016-02-18 14:32:00 +1100

    Add constrator function to searchbar component to initialize state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+  // for state we need constractor function
+
+  constractor(props){
+    super(props);
+
+    // initialize the state of term as a empty string
+    this.state = { term: ''};
+  }
   render() {
 
     // return < input onCange = { event => console.log(event.target.value)};
