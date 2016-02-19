d8bbfd33c6888cc68660bf608c28780001b99e95
Tanim Mahmud <tanimmahmud08@gmail.com>
2016-02-19 17:48:13 +1100

    import youtube search api pachage to index page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTsearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar'
 
-const API_KEY = 'AIzaSyB8dOiDqQd0rm2YuzycbYUNr6V1flhGkQg';
+const API_KEY = 'AIzaSyBwX32dIfXXgmEHl2BjSheEJ7DcMA3ghHM';
+
+YTsearch({key: API_KEY, term: 'bangla movie'}, (data)=>{
+  console.log(data);
+});
 
 const App = () => {
   return <div>
