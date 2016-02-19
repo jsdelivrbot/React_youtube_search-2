0adaf39df58d38d93d5fb41200a251b3db70c92a
Tanim Mahmud <tanimmahmud08@gmail.com>
2016-02-19 17:35:42 +1100

    Set input value as current state of data

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,22 +4,28 @@ class SearchBar extends React.Component {
 
   // for state we need constractor function
 
-  constractor(props){
+  constructor(props){
     super(props);
 
     // initialize the state of term as a empty string
-    this.state = { term: ''};
+    this.state = { term: 'Search Here'};
   }
+
   render() {
 
-    // return < input onCange = { event => console.log(event.target.value)};
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange = { event => this.setState({ term: event.target.value})} />
+      </div>)
 
-    return < input onChange={this.onInputChange}/>;
+    // return < input onChange={this.onInputChange}/>;
   }
 
-  onInputChange(event){
-    console.log(event.target.value);
-  }
+  // onInputChange(event){
+  //   console.log(event.target.value);
+  // }
 }
 
 export default SearchBar;
