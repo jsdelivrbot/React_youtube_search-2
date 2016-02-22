d503b75529daadbad8219dbac8d2c765b3796c31
Tanim Mahmud <tanimmahmud08@gmail.com>
2016-02-23 00:22:14 +1100

    import video detail

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTsearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBwX32dIfXXgmEHl2BjSheEJ7DcMA3ghHM';
 
@@ -23,6 +24,7 @@ class App extends Component {
     return(
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos}/>
       </div>
     );
